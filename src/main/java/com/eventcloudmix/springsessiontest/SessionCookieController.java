a0d389d70ce3bf21b9ce310f9ef68847481ffc2c
Satoru Hashimoto <satoru.hashimoto@majors.co.jp>
2024-07-09 20:01:20 +0900

    cleaning

--- src/main/java/com/eventcloudmix/springsessiontest/SessionCookieController.java
+++ src/main/java/com/eventcloudmix/springsessiontest/SessionCookieController.java
@@ -1,7 +1,6 @@
 package com.eventcloudmix.springsessiontest;
 
 import jakarta.servlet.http.HttpSession;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,39 +9,26 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Controller
-@Slf4j
 public class SessionCookieController {
 
-    public static final String HELLO_IN_JA = "こんにちは";//15byte
-    public static final String HELLO_IN_EN = "hellohellohello";//15byte
-
-    @GetMapping("/")
-    public String index(HttpSession session, Model model){
-        log.info("Session id:{}", session.getId());
-        model.addAttribute("html", HELLO_IN_JA);
-        return "index";
-    }
+    public static final String HELLO_IN_JA = "こんにちは"; //15byte
 
     @GetMapping("/cookie")
     public String cookie(HttpSession session, Model model){
-        log.info("Session id:{}", session.getId());
         // < 32768byte doc
-        String html = IntStream.range(0, 2179)
+        String html = IntStream.range(0, 2184)
                 .mapToObj(i -> HELLO_IN_JA)
                 .collect(Collectors.joining());
-
         model.addAttribute("html", html);
         return "index";
     }
 
     @GetMapping("/nocookie")
     public String nocookie(HttpSession session, Model model){
-        log.info("Session id:{}", session.getId());
         // > 32768byte doc
-        String html = IntStream.range(0, 2180)
+        String html = IntStream.range(0, 2185)
                 .mapToObj(i -> HELLO_IN_JA)
                 .collect(Collectors.joining());
-
         model.addAttribute("html", html);
         return "index";
     }
