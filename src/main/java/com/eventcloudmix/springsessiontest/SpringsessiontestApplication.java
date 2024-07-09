af8c614c0f773af2a254aaae27e99eccfbe23d0c
Satoru Hashimoto <satoru.hashimoto@majors.co.jp>
2024-07-09 10:59:29 +0900

    init

---
+++ src/main/java/com/eventcloudmix/springsessiontest/SpringsessiontestApplication.java
@@ -0,0 +1,13 @@
+package com.eventcloudmix.springsessiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsessiontestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsessiontestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventcloudmix/springsessiontest/SpringsessiontestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventcloudmix.springsessiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsessiontestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
